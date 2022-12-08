2b3eadf74d1a409a348ea0eb055871ca5f6355df
Rijiy9013 <makcik1337@gmail.com>
2022-12-08 14:50:12 +0400

    1.0 версия

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import tkinter as tk
 from tkinter import ttk
 
@@ -6,22 +7,18 @@ class MyProgram:
     window = tk.Tk()
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    marks = [['a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'q3', 'q5', 'q7', 'q8'],
-            ['golf', 'polo', 'passat', 'tiguan', 'touareg']]
-    emissions = [['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'], ['1', '1', '1', '1', '1']]
-    brands = [['audi'], ['volkswagen']]
-    brands_lst = ['audi', 'volkswagen']
-    shortsBrands = [['ауди', 'aud', 'auid'], ['vk', 'volkwagen', 'vlokswagen', 'wagen', 'volks']]
-    shortsMarks = [['а2', 'а3', 'а4', 'а5', 'а6', 'а7', 'а8'], ['гольф', 'голф', 'поло', 'полик', 'пассат']]
+    wait_var = tk.IntVar()
+    brands = []
     dicCars = {}
     dicEmis = {}
     mark_from_user = ''
     brand_from_user = ''
-    model_from_user = ''
-    wait_var = tk.IntVar()
+    engine_size = 0
+    avg_km_per_year = 20000
+    norm_of_co2 = 130
 
     def __init__(self):
-        self.window.title('Тест')
+        self.window.title('БЖД')
         self.window.geometry(f'{self.screen_width}x{self.screen_height}')
         app_width = 700
         app_height = 400
@@ -38,13 +35,41 @@ class MyProgram:
         MyProgram.window.mainloop()
 
     def update_dic(self):
-        for i in range(len(self.brands)):
-            self.dicCars.update(dict.fromkeys(self.brands[i], self.marks[i]))
-            self.dicEmis.update(dict.fromkeys(self.marks[i], self.emissions[i][i]))
+        with open("CO2_Emissions_Canada.csv", mode="r") as file:
+            reader = list(csv.reader(file))
+            temp = []
+            flag = len(reader) - 1
+            i = 1 #7-11
+            while flag:
+                j = i
+                temp = []
+                while reader[i][0] == reader[j][0]:
+                    self.brands.append(reader[i][0])
+                    if reader[j][1] not in temp and "HYBRID" not in reader[j][1].upper():
+                        temp.append(reader[j][1])
+                    flag -= 1
+                    j += 1
+                    if not flag:
+                        break
+                self.dicCars[reader[i][0]] = temp
+                i = j
+            self.brands = (list(dict.fromkeys(self.brands)))
+
+    def get_emissions(self):
+        lst = []
+        with open("CO2_Emissions_Canada.csv", mode="r") as file:
+            reader = list(csv.reader(file))
+            for i in range(len(reader)):
+                if reader[i][0].lower() == self.brand_from_user.lower() and reader[i][1].lower() == self.mark_from_user.lower() and reader[i][3].lower() == self.engine_size:
+                    lst.append(reader[i][7])
+                    lst.append(reader[i][8])
+                    lst.append(reader[i][9])
+                    lst.append(reader[i][10])
+                    lst.append(reader[i][11])
+                    return lst
 
     def make_main_buttons(self):
-        # lbl = tk.Label(self.window, text=(', '.join(map(str, self.dic['audi']))))
-        # lbl.grid(row=0, column=0)
+        self.engine_size = 0
         brand_label = tk.Label(self.window, text='Напишите бренд Вашей машины: ')
         brand_label.grid(row=0, column=0)
         self.combo_box_brand['values'] = self.brands
@@ -54,45 +79,22 @@ class MyProgram:
         brand_label.grid(row=1, column=0)
         self.combo_box_mark.configure(state='disabled')
         self.window.wait_variable(self.wait_var)
+        self.combo_box_brand.unbind('<KeyRelease>')
         brand_label.configure(state='active')
         self.combo_box_mark.configure(state='active')
         self.combo_box_mark['values'] = self.dicCars[self.brand_from_user]
         self.combo_box_mark.bind('<KeyRelease>', self.check_input_mark)
         self.combo_box_mark.grid(row=1, column=1)
-        # brand_entry = tk.Entry(self.window)
-        # brand_entry.grid(row=0, column=1)
-        # wait_var = tk.IntVar()
-        # self.window.bind("<Return>", lambda event: wait_var.set(1))
-        # self.window.wait_variable(wait_var)
-        # self.window.unbind("<Return>")
-        # self.brand_from_user = self.get_brand(brand_entry.get())
-        # if self.mark_from_user != '':
-        #     self.check_car()
-        # else:
-        #     brand_label.configure(state='disabled')
-        #     mark_label = tk.Label(self.window, text='Напишите марку Вашей машины: ')
-        #     mark_label.grid(row=1, column=0)
-        #     mark_entry = tk.Entry(self.window)
-        #     mark_entry.grid(row=1, column=1)
-        #     temp_var = tk.IntVar()
-        #     self.window.bind("<Return>", lambda event: temp_var.set(1))
-        #     self.window.wait_variable(temp_var)
-        #     self.window.unbind("<Return>")
-        #     self.mark_from_user = self.get_mark(mark_entry.get())
-        #     if self.mark_from_user != '':
-        #         mark_label.configure(state='disabled')
-        #         self.check_car()
 
     def check_input_brand(self, event):
         value = event.widget.get()
         if value == '':
-            self.combo_box_brand['values'] = self.brands_lst
+            self.combo_box_brand['values'] = self.brands
         else:
             data = []
-            for item in self.brands_lst:
+            for item in self.brands:
                 if value.lower() in item.lower():
                     data.append(item)
-
             self.combo_box_brand['values'] = data
         if self.get_brand(self.combo_box_brand.get()) != '':
             self.brand_from_user = self.get_brand(self.combo_box_brand.get())
@@ -108,7 +110,6 @@ class MyProgram:
             for item in self.dicCars[self.brand_from_user]:
                 if value.lower() in item.lower():
                     data.append(item)
-
             self.combo_box_mark['values'] = data
         if self.get_mark(self.combo_box_mark.get()) != '':
             self.mark_from_user = self.get_mark(self.combo_box_mark.get())
@@ -116,43 +117,48 @@ class MyProgram:
                 self.check_car()
 
     def get_brand(self, arg):
-        if ' ' in arg.lower(): #если пробел, чтобы считать и марку, и модель
-            for i, val in enumerate(arg.lower()):
-                if val == ' ' and i != len(arg.lower()):
-                    self.mark_from_user = self.get_mark(arg.lower()[i+1:])
-                    arg = arg.lower()[:i]
         for i in range(len(self.brands)): #проверка бренд без опечаток
-            if arg.lower() in self.brands[i]:
+            if arg.lower() in self.brands[i].lower():
                 return arg
-        for i in range(len(self.shortsBrands)): #проверка на опечатку
-            if arg.lower() in self.shortsBrands[i]:
-                return self.brands[i][0]
 
     def get_mark(self, arg):
-        for i in range(len(self.marks)): #проверка модели без опечаток
-            if arg.lower() in self.marks[i]:
+        for i in range(len(self.dicCars[self.brand_from_user])): #проверка модели без опечаток
+            if arg.lower() in self.dicCars[self.brand_from_user][i].lower():
                 return arg
-        for i in range(len(self.shortsMarks)): #проверка на опечатку не сделана
-            if arg.lower() in self.shortsMarks[i]:
-                return self.marks[i][0]
 
     def check_car(self):
         self.combo_box_brand.unbind('<KeyRelease>')
         self.combo_box_mark.unbind('<KeyRelease>')
+        engines = self.check_several_engines()
         temp = tk.IntVar()
+        wait = tk.IntVar()
         check_win = tk.Toplevel(self.window)
         check_win.wm_title('Проверка')
         check_win.resizable(False, False)
-        app_width = 250
+        app_width = 550
         app_height = 125
         x = (self.screen_width / 2) - (app_width / 2)
         y = (self.screen_height / 2) - (app_height / 2)
         check_win.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
-        tk.Label(check_win, text=self.brand_from_user + ' ' + self.mark_from_user + ' - это Ваша машина?').grid(row=0, column=0)
+        if len(engines) > 1:
+            tk.Label(check_win, text='Выберите объем двигателя:').grid(
+                row=0, column=0)
+            variableObj = tk.StringVar(check_win)
+            variableObj.set(engines[0])
+            objectMenu = tk.OptionMenu(check_win, variableObj, *engines)
+            objectMenu.grid(row=0, column=1)
+            check_win.bind('<KeyRelease>', lambda event: wait.set(1))
+            check_win.wait_variable(wait)
+            check_win.unbind('<KeyRelease>')
+            self.engine_size = variableObj.get()
+
+        if not self.engine_size:
+            self.engine_size = engines[0]
+        tk.Label(check_win, text=self.brand_from_user + ' ' + self.mark_from_user + ' ' + str(self.engine_size) + ' Л' +  ' - это Ваша машина?').grid(row=1, column=0)
         yes_btn = tk.Button(check_win, text='Да', command=lambda: temp.set(1))
-        yes_btn.grid(row=1, column=0)
+        yes_btn.grid(row=2, column=0)
         no_btn = tk.Button(check_win, text='Нет', command=lambda: temp.set(2))
-        no_btn.grid(row=1, column=1)
+        no_btn.grid(row=2, column=1)
         check_win.wait_variable(temp)
         if temp.get() == 1:
             check_win.destroy()
@@ -174,11 +180,43 @@ class MyProgram:
             check_win.destroy()
             self.make_main_buttons()
 
+    def check_several_engines(self):
+        lst = []
+        with open("CO2_Emissions_Canada.csv", mode="r") as file:
+            reader = list(csv.reader(file))
+            for i in range(len(reader)):
+                if reader[i][0].lower() == self.brand_from_user.lower() and reader[i][
+                    1].lower() == self.mark_from_user.lower():
+                    lst.append(reader[i][3])
+        return list(dict.fromkeys(lst))
 
     def show_info(self):
         self.combo_box_brand.configure(state='disabled')
         self.combo_box_mark.configure(state='disabled')
-        tk.Label(self.window, text=self.dicEmis[str(self.mark_from_user)]).grid(row=2, column=0)
+        temp = self.get_emissions()
+        tk.Label(self.window, text='Средний расход в городе: ' + temp[0]).grid(row=4, column=0)
+        tk.Label(self.window, text='Средний расход на трассе: ' + temp[1]).grid(row=5, column=0)
+        tk.Label(self.window, text='Средний расход смешанный: ' + temp[2]).grid(row=6, column=0)
+        tk.Label(self.window, text='Средний расход количество выбросов СО2: ' + temp[4]).grid(row=7, column=0)
+        if int(temp[4]) - self.norm_of_co2 > 0:
+            tk.Label(self.window, text='Превышение выбросов СО2 на: ' + str(int(temp[4]) - self.norm_of_co2)).grid(row=8, column=0)
+        km_anually = self.norm_of_co2 * self.avg_km_per_year / int(temp[4])
+        tk.Label(self.window, text='Безопасное количество километров в год: ' + str(round(km_anually, 1))).grid(row=9, column=0)
+        tk.Button(self.window, text='Сброс', command=self.clear_all).grid(row=10, column=1)
+
+    def clear_all(self):
+        self.dicCars.clear()
+        self.brands.clear()
+        self.brand_from_user = ''
+        self.mark_from_user = ''
+        self.engine_size = 0
+        self.wait_var.set(0)
+        for widget in self.window.winfo_children():
+            widget.destroy()
+        self.combo_box_brand = ttk.Combobox(self.window)
+        self.combo_box_mark = ttk.Combobox(self.window)
+        self.update_dic()
+        self.make_main_buttons()
 
 
 program = MyProgram()
